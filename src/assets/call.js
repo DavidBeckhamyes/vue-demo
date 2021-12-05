d5f0917264af0b3f720940f26108271b72b7c302
DavidBeckhamyes <zhan19921120@163.com>
2021-12-05 21:58:01 +0800

    bind方法添加

--- src/assets/call.js
+++ src/assets/call.js
@@ -7,6 +7,8 @@ function Person(a, b, c, d) {
     }
 }
 
+Person.prototype.collection = '收藏';
+
 var egg = {
     name: '蛋老师',
 }
@@ -43,5 +45,35 @@ Function.prototype.newApply = function (obj, arr) {
     return result;
 }
 
-console.log(Person.newCall(egg, '点赞', '投币', '转发', '充电'));
+Function.prototype.newBind = function (obj) {
+    if (typeof this != 'function') {
+        throw new TypeError('错误!');
+    }
+    // that保存好this对象(Person函数)
+    var that = this;
+    // Array.prototype.slice.call(arguments)能将具有length属性的对象(key值为数字)转成数组。
+    arr = Array.prototype.slice.call(arguments, 1);
+    o = function() {}
+    newf = function () {
+        var arr2 = Array.prototype.slice.call(arguments);
+        arrSum = arr.concat(arr2);
+        if (this instanceof newf) {
+            // 如果使用了new来调用函数 也就是发生了构造函数 那么实例(新对象)会绑定到函数调用的this上
+            // 此时的this不是person函数，而是一个实例，也就是发生了new构造函数
+            that.apply(this, arrSum);
+        } else {
+            that.apply(obj, arrSum);
+        }
+    }
+    o.prototype = that.prototype;
+    newf.prototype = new o;
+    return newf
+}
+
+// console.log(Person.newCall(egg, '点赞', '投币', '转发', '充电'));
 // console.log(Person.newApply(egg, ['点赞', '投币', '转发', '充电']));
+// Person.newBind(egg, "点赞", "收藏")("投币", "转发");
+var bibi = Person.newBind(egg, "点赞", "投币");
+// 加入new会使得this指向失效
+var b = new bibi("转发", "充电")
+console.log(b.collection);
