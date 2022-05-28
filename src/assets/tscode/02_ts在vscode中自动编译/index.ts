28aa5c9ddbc0fa07b7538eaee9e9f1c48fde7b46
詹凌瀚 <zhanlinghan@rails.cn>
2022-05-28 19:07:16 +0800

    ts初识

---
+++ src/assets/tscode/02_ts在vscode中自动编译/index.ts
@@ -0,0 +1,8 @@
+(() => {
+   function sayHi(str) {
+       return "安妮哈赛有" + str;
+   }
+   let str = "小甜甜";
+   console.log(sayHi(str));
+   
+})()
---
+++ src/assets/tscode/02_ts在vscode中自动编译/js/index.js
@@ -0,0 +1,7 @@
+(() => {
+    function sayHi(str) {
+        return "安妮哈赛有" + str;
+    }
+    let str = "小甜甜";
+    console.log(sayHi(str));
+})();
