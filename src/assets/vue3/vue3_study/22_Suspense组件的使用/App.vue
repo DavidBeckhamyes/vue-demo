2b2a932b6220fde767ccff58abe02b75664548cc
DavidBeckhamyes <zhan19921120@163.com>
2022-12-18 22:36:21 +0800

    分包

---
+++ src/assets/vue3/vue3_study/22_Suspense组件的使用/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 它们允许我们的应用程序在等待异步组件时渲染一些后备内容，可以让我们创建一个平滑的用户体验 -->
+
+  <Suspense>
+    <template v-slot:default>
+      <!-- <AsyncComp /> -->
+      <AsyncAddress />
+    </template>
+
+    <template v-slot:fallback>
+      <h1>LOADING...</h1>
+    </template>
+  </Suspense>
+</template>
+
+<script lang="ts">
+/*
+异步组件 + Suspense组件
+*/
+import AsyncAddress from "./components/AsyncAddress.vue";
+import { defineComponent, defineAsyncComponent } from "vue";
+// const AsyncComp = defineAsyncComponent(
+//   () => import("./components/AsyncComp.vue")
+// );
+export default defineComponent({
+  setup() {
+    return {};
+  },
+
+  components: {
+    AsyncAddress,
+  },
+});
+</script>
---
+++ src/assets/vue3/vue3_study/22_Suspense组件的使用/components/AsyncAddress.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{ data }}</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+export default defineComponent({
+  async setup() {
+    const result = await axios.get("/data/address.json");
+    return {
+      data: result.data,
+    };
+  },
+});
+</script>
