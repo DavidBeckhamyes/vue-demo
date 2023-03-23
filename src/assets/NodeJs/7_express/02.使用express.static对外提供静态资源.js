afa84a6e22baba4e6d320c3733bd0600214b3d86
DavidBeckhamyes <zhan19921120@163.com>
2023-03-23 21:54:11 +0800

    express初级

---
+++ src/assets/NodeJs/7_express/02.使用express.static对外提供静态资源.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// 可直接访问 file 目录下的静态资源
+// app.use(express.static('../file'));
+
+// 通过带有 /file 前缀的地址访问 file 目录下的文件
+app.use('/file', express.static('../file'))
+
+app.listen('8080', () => {
+    console.log("express server running at http://127.0.0.1");
+})
