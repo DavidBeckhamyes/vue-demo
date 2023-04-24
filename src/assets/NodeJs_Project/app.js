ccc780c31fef1d71818b9ae8118b865232f73bb4
詹凌瀚 <zhanlinghan@rails.cn>
2023-04-24 17:54:53 +0800

    修改

--- src/assets/NodeJs_Project/app.js
+++ src/assets/NodeJs_Project/app.js
@@ -9,7 +9,7 @@ app.use(cors())
 // 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
-// 响应数据的中间件
+// 响应数据的全局中间件
 app.use(function (req, res, next) {
     // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
     res.cc = function (err, status = 1) {
@@ -26,6 +26,31 @@ app.use(function (req, res, next) {
 // 导入注册用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
+
+// 定义全局错误级别中间件，捕获验证失败的错误，并把验证失败的结果响应给客户端
+const joi = require('joi')
+app.use(function (err, req, res, next) {
+    // 数据验证失败
+    if (err instanceof joi.ValidationError) return res.cc(err)
+    // 捕获身份认证失败的错误
+    if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+    // 未知错误
+    res.cc(err)
+})
+
+// 导入配置文件
+const config = require('./config')
+
+// 解析 token 的中间件
+const expressJWT = require('express-jwt')
+
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] }))
+
+// 导入并使用用户信息路由模块
+const userinfoRouter = require('./router/userinfo')
+// 注意：以 /my 开头的接口，都是有权限的接口，需要进行 Token 身份认证
+app.use('/my', userinfoRouter)
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(3007, function () {
--- src/assets/NodeJs_Project/router_handler/user.js
+++ src/assets/NodeJs_Project/router_handler/user.js
@@ -6,6 +6,9 @@ const db = require('../db/index');
 
 const bcrypt = require('bcryptjs');
 
+// 用这个包来生成 Token 字符串
+const jwt = require('jsonwebtoken')
+
 // 注册用户的处理函数
 exports.regUser = (req, res) => {
     // 接收表单数据
@@ -56,5 +59,42 @@ exports.regUser = (req, res) => {
 
 // 登录的处理函数
 exports.login = (req, res) => {
-    res.send('login OK')
+    // 接收表单数据
+    const userinfo = req.body
+    // 定义 SQL 语句：
+    const sql = "select * from ev_users where username=?";
+
+    db.query(sql, userinfo.username, (err, results) => {
+        // 执行 SQL 语句失败
+        if (err) return res.cc(err)
+        // 执行 SQL 语句成功，但是查询到数据条数不等于 1
+        if (results.length !== 1) return res.cc('登录失败！')
+        // TODO：判断用户输入的登录密码是否和数据库中的密码一致
+        // 拿着用户输入的密码,和数据库中存储的密码进行对比
+        const compareResult = bcrypt.compareSync(userinfo.password, results[0].password)
+
+        // 如果对比的结果等于 false, 则证明用户输入的密码错误
+        if (!compareResult) {
+            return res.cc('登录失败！')
+        }
+
+        // TODO：登录成功，生成 Token 字符串
+        // 剔除完毕之后，user 中只保留了用户的 id, username, nickname, email 这四个属性的值
+        const user = { ...results[0], password: '', user_pic: '' };
+
+        // 导入配置文件
+        const config = require('../config')
+
+        // 生成 Token 字符串
+        const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+            expiresIn: '10h', // token 有效期为 10 个小时
+        })
+
+        // 将生成的 Token 字符串响应给客户端
+        res.send({
+            status: 0,
+            message: '登录成功！',
+            token: tokenStr,
+        })
+    })
 }
