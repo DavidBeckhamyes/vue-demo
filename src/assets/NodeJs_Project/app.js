3601935b3db9c60e76b9585cd2ac2cc38a04ac74
詹凌瀚 <zhanlinghan@rails.cn>
2023-04-26 17:59:34 +0800

    剩余项目

--- src/assets/NodeJs_Project/app.js
+++ src/assets/NodeJs_Project/app.js
@@ -9,6 +9,9 @@ app.use(cors())
 // 配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
+// 托管静态资源文件
+app.use('/uploads', express.static('./uploads'))
+
 // 响应数据的全局中间件
 app.use(function (req, res, next) {
     // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
@@ -51,6 +54,16 @@ app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api\//] 
 const userinfoRouter = require('./router/userinfo')
 // 注意：以 /my 开头的接口，都是有权限的接口，需要进行 Token 身份认证
 app.use('/my', userinfoRouter)
+
+// 导入并使用文章分类路由模块
+const artCateRouter = require('./router/artcate')
+// 为文章分类的路由挂载统一的访问前缀 /my/article
+app.use('/my/artcate', artCateRouter)
+
+// 导入并使用文章路由模块
+const articleRouter = require('./router/article')
+// 为文章的路由挂载统一的访问前缀 /my/article
+app.use('/my/article', articleRouter)
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(3007, function () {
